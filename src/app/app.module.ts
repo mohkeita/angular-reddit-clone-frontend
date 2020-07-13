103854b3f7817cd5f150e7dcb14ea84003c5f02b
mohkeita <mohkeita13@gmail.com>
2020-07-13 16:13:27 +0200

    Improvements to Login and Signup process and implement Refresh Token Handling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,26 +6,38 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import {ReactiveFormsModule} from "@angular/forms";
-import {HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import { LoginComponent } from './auth/login/login.component';
 import {NgxWebstorageModule} from "ngx-webstorage";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {ToastrModule} from "ngx-toastr";
+import { HomeComponent } from './home/home.component';
+import {TokenInterceptor} from "./token-interceptor";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SignupComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
-    NgxWebstorageModule.forRoot()
-
+    NgxWebstorageModule.forRoot(),
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -5,12 +5,17 @@ import {Observable} from "rxjs";
 import {LocalStorageService} from "ngx-webstorage";
 import {LoginRequestPayload} from "../login/login.request.payload";
 import {LoginResponse} from "../login/login-response.payload";
-import {map} from "rxjs/operators";
+import {map, tap } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  refreshTokenPayload = {
+    refreshToken: this.getRefreshToken(),
+    username: this.getUserName()
+  }
 
   constructor(private httpClient: HttpClient,
               private localStorage: LocalStorageService) { }
@@ -31,4 +36,29 @@ export class AuthService {
         return true;
     }));
   }
+
+  getJwtToken() {
+    return this.localStorage.retrieve('authenticationToken');
+  }
+
+  refreshToken() {
+    return this.httpClient.post<LoginResponse>('http://localhost:8080/api/auth/refresh/token',
+      this.refreshTokenPayload)
+      .pipe(tap(response => {
+        this.localStorage.clear('authenticationToken');
+        this.localStorage.clear('expiresAt');
+
+        this.localStorage.store('authenticationToken',
+          response.authenticationToken);
+        this.localStorage.store('expiresAt', response.expiresAt);
+      }));
+  }
+
+  getUserName() {
+    return this.localStorage.retrieve('username');
+  }
+  getRefreshToken() {
+    return this.localStorage.retrieve('refreshToken');
+  }
+
 }
